3e55d436f37ba1816ce9fb8c3c096505d9041a2d
Yaroslav Andrianov <yaropolk1995@gmail.com>
2025-01-13 06:53:11 -0600

    modified:   dev/compile.bat

--- dev/compile.bat
+++ dev/compile.bat
@@ -1,4 +1,4 @@
 echo on
-pyinstaller --name="Statistics" --onefile --windowed --clean --icon="..\src\SigmaStatistics\icon.ico" --splash splash.png --optimize 0 --upx-dir upx ..\src\SigmaStatistics\build.py
+pyinstaller --name="SigmaStatistics" --onefile --windowed --clean --icon="..\src\SigmaStatistics\icon.ico" --splash splash.png --optimize 0 --upx-dir upx ..\src\SigmaStatistics\build.py
 @REM dont use optimization, it causes ignoring assertions in python code
 pause
